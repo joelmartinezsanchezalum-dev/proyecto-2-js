97b7797848419fb443a03d94a15763db809f8861
Joel Martinez <joelmartinezsanchez.alum@insestatut.cat>
2026-01-26 16:32:30 +0100

    funcion carpeta juego

--- proyecto.js
+++ proyecto.js
@@ -1,26 +1,34 @@
 const prompt = require("prompt-sync")({ sigint: true })
 const fs = require("fs");
 
-function llamada(valor1) {
+//funcion que enseña los tableros de juego 
+function carpetajuego() {
+    console.log("Llista de taulers disponibles: ");
+    console.log(fs.readdirSync("./tauler").toString);
+    let nomtablerouser = prompt("Introdueix la teva opcio");
+}
 
+
+function llamada(valor1) {
     if (valor1 == 0) {
         console.clear();
         console.log("Has seleccionat sortir del programa");
         bucle = false;
     } else if (valor1 == 1) {
-        console.log("Llista de taulers disponibles: ");
-        fs.mkdir
-        console.log(fs.readdirSync("./tauler").toString);
-        let nomtablerouser = prompt("Introdueix la teva opcio");
-        fs.existsSync("./" + nomtablerouser)    
-    } else if (valor1 == 2) {
+        if (fs.existsSync("./tauler")) { carpetajuego("") }
+        else {
+            fs.mkdir("./tauler")
+            carpetajuego("")
+        }
+    }
+    else if (valor1 == 2) {
 
     } else if (valor1 == 3) {
 
     } else if (valor1 == 4) {
 
-    } else { 
-        console.log ("Error, esta opcion no existe, pruebe de nuevo")
+    } else {
+        console.log("Error, esta opcion no existe, pruebe de nuevo")
     }
 }
 
@@ -35,7 +43,7 @@ while (bucle == true) {
     console.log("3- estadisticas tableros")
     console.log("4- crear tablero nuevo")
     console.log("----------------")
-    let menuprincipal = Number.parseInt(prompt("Introdueix la teva opcio"));
+    let menuprincipal = Number.parseInt(prompt("Introdueix la teva opcio "));
     llamada(menuprincipal);
 }
 
